e6ca7a8a4475c4f5a78926201479149a3f842aa5
Max <max@maxf.net>
2025-04-15 19:49:21 -0400

    updated

---
+++ fedora/41/cloud-init.d/02-user-dpsrv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+user=dpsrv
+user_home=/mnt/data/$user
+
+useradd -G docker -d $user_home $user
+
+mkdir -p ~$user/.ssh
+
+cp /root/.ssh/authorized_keys $user_home/.ssh/
+chmod -R og-rwx $user_home/.ssh/
+
+cp -r ~root/{.config,.gitconfig,.git-credentials} $user_home/
+
+chown -R $user:$user $user_home/
+
+sudo -u $user init-user-projects.sh
---
+++ fedora/41/init-user-projects.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ex
+user=$USER
+
+user_home=/mnt/data/$user
+cd $user_home
+
+git config --global credential.helper store
+
+git clone https://github.com/maxfortun/git-openssl-secrets.git
+
+cd git-openssl-secrets
+ln -s git-setenv-openssl-secrets-fs.sh git-setenv-openssl-secrets.sh
+cd ..
+
+git clone https://github.com/$user/rc.git
+cd rc
+../git-openssl-secrets/git-init-openssl-secrets.sh
+cd ..
+
+mkdir $user_home/.bashrc.d
+
+if [ "$user" != "dpsrv" ]; then
+	echo ". /mnt/data/dpsrv/rc/bin/dpsrv.sh" > $user_home/.bashrc.d/00-dpsrv.sh
+	. /mnt/data/dpsrv/rc/bin/dpsrv.sh
+fi
+
+echo ". $user_home/rc/bin/$user.sh" > $user_home/.bashrc.d/01-$user.sh
+. rc/bin/$user.sh
+
+$user-git-clone
+$user-git-init-secrets
+
+$user-up
+
