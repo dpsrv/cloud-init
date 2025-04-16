2df56387cfcb7f6644490e5a2ca9f674ad7565ba
Max <max@maxf.net>
2025-04-15 20:24:05 -0400

    updated

--- fedora/41/cloud-init.d/03-user-dpsrv.sh
+++ fedora/41/cloud-init.d/03-user-dpsrv.sh
@@ -15,3 +15,8 @@ cp -r ~root/{.config,.gitconfig,.git-credentials} $user_home/
 chown -R $user:$user $user_home/
 
 sudo -u $user ./init-user-projects.sh
+
+chmod a+rx $user_home/
+chmod -R a+r $user_home/rc/
+
+find $user_home/rc/ -type d -exec chmod a+x {} \;
