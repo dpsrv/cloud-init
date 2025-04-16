880afa9b65ced59e382bd1ec6ad7db8d5e32b784
Max <max@maxf.net>
2025-04-16 16:27:37 -0400

    updated

--- fedora/41/cloud-init.d/03-network.sh
+++ fedora/41/cloud-init.d/03-network.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -ex
 
-resolved=$(host "$(hostname -s).dpsrv.me")
-if floating_ip=$(echo "$resolved"|awk '{ print $4 }'); then
-
-    if ! ip a s | grep -q $floating_ip; then
-        ip addr add $floating_ip dev eth0
-    fi
-
+if [ -n "$FLOATING_IP_IF" ]; then
+	resolved=$(host "$(hostname -s).dpsrv.me")
+	if floating_ip=$(echo "$resolved"|awk '{ print $4 }'); then
+	
+    	if ! ip a s | grep -q $floating_ip; then
+        	ip addr add $floating_ip dev $FLOATING_IP_IF
+    	fi
+	
+	fi
 fi
 
