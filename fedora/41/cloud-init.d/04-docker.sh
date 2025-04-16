5f056e27f0407ed024ed0b41cfca003b16ec7e93
Max <max@maxf.net>
2025-04-16 16:19:36 -0400

    updated

--- fedora/41/cloud-init.d/04-docker.sh
+++ fedora/41/cloud-init.d/04-docker.sh
@@ -2,9 +2,11 @@
 
 dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+mkdir /mnt/docker-data
 
 cat >> /etc/docker/daemon.json << _EOT_
 {
+	"data-root": "/mnt/docker-data",
     "log-driver": "json-file",
     "log-opts": {
         "max-size": "10m",
