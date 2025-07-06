9c10bd84954ec0f70e2eefeaa937399b2c8aa8b0
Max <max@maxf.net>
2025-07-06 12:36:47 -0400

    updated

--- fedora/41/cloud-init.d/05-user-dpsrv.sh
+++ fedora/41/cloud-init.d/05-user-dpsrv.sh
@@ -2,15 +2,16 @@
 
 user=dpsrv
 user_home=/mnt/data/$user
+DPSRV_CFG_SRC_D=${DPSRV_CFG_SRC_D:-/root}
 
 useradd -G docker -d $user_home $user
 
 mkdir -p $user_home/.ssh
 
-cp /root/.ssh/authorized_keys $user_home/.ssh/
+cp $DPSRV_CFG_SRC_D/.ssh/authorized_keys $user_home/.ssh/
 chmod -R og-rwx $user_home/.ssh/
 
-cp -r ~root/{.config,.gitconfig,.git-credentials} $user_home/
+cp -r $DPSRV_CFG_SRC_D/{.config,.gitconfig,.git-credentials} $user_home/
 
 chown -R $user:$user $user_home/
 
