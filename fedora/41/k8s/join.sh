55aa9ec7be06693798923c47e0fe3ea89e381d27
Max <max@maxf.net>
2025-09-23 04:18:31 +0000

    updated

--- fedora/41/k8s/join.sh
+++ fedora/41/k8s/join.sh
@@ -16,3 +16,31 @@ metallb_ips=$( echo $ROUTABLE_IPS| tr ' ' '\n' | sed 's/^/\"/;s/$/\/32\",/' | tr
 kubectl -n metallb-system get ipaddresspool default -o json \
 	| jq ".spec.addresses += [$metallb_ips] | .spec.addresses |= unique" \
 	| kubectl apply -f -
+
+metallb_ips=( $ROUTABLE_IPS )
+metallb_ips_count=${#metallb_ips[*]}
+for (( i=0; i < $metallb_ips_count; i++ )) ; do
+	metallb_ip=${ROUTABLE_IPS[$i]}
+	svcId=$K8S_NODE_NAME
+	[ "$metallb_ips_count" = "1" ] || svcId="$svcId-$i"
+
+	cat <<_EOT_ | kubectl apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: istio-ingressgateway-$svcId
+  namespace: istio-system
+spec:
+  type: LoadBalancer
+  loadBalancerIP: $metallb_ip
+  selector:
+    app: istio-ingressgateway
+    istio: ingressgateway
+  ports:
+    - port: 80
+      targetPort: 80
+    - port: 443
+      targetPort: 443
+_EOT_
+
+done
