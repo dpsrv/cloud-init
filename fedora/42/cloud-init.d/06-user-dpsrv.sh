4fb9741ca1a12f758159ca2bf1cd73fec531a484
Max <max@maxf.net>
2026-04-18 18:51:46 -0400

    updated

--- fedora/42/cloud-init.d/06-user-dpsrv.sh
+++ fedora/42/cloud-init.d/06-user-dpsrv.sh
@@ -9,12 +9,10 @@ usermod -aG k3s $user
 
 mkdir -p $user_home/.ssh
 
-cp $DPSRV_CFG_SRC_D/.ssh/authorized_keys $user_home/.ssh/
-chmod -R og-rwx $user_home/.ssh/
-
-cp -r $DPSRV_CFG_SRC_D/{.config,.gitconfig,.git-credentials,.docker} $user_home/
+cp -r $DPSRV_CFG_SRC_D/{.config,.gitconfig,.git-credentials,.docker,.ssh} $user_home/
 
 chown -R $user:$user $user_home/
+chmod -R og-rwx $user_home/.ssh/
 
 docker network create $user
 docker network ls
--- fedora/42/cloud-init.d/07-user-ezsso.sh
+++ fedora/42/cloud-init.d/07-user-ezsso.sh
@@ -7,13 +7,9 @@ DPSRV_CFG_SRC_D=${DPSRV_CFG_SRC_D:-/root}
 useradd -G docker -d $user_home $user
 usermod -aG k3s $user
 
-mkdir -p $user_home/.ssh
-
-cp $DPSRV_CFG_SRC_D/.ssh/authorized_keys $user_home/.ssh/
-chmod -R og-rwx $user_home/.ssh/
-
-cp -r $DPSRV_CFG_SRC_D/{.config,.gitconfig,.git-credentials,.docker} $user_home/
+cp -r $DPSRV_CFG_SRC_D/{.config,.gitconfig,.git-credentials,.docker,.ssh} $user_home/
 
 chown -R $user:$user $user_home/
+chmod -R og-rwx $user_home/.ssh/
 
 sudo -u $user ./init-user-projects.sh
